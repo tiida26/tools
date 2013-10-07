c4fc02772d5efe4f24c61eefdca3cf55dba78105
tiida26 <ambo.sun@gmail.com>
2013-10-07 18:59:02 +0900

    むだな改行を消したよ！

--- backupfilename.sh
+++ backupfilename.sh
@@ -6,24 +6,20 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 export PATH
 
 
-
 # Variables 01
 DATE=`date +%Y%m%d`
 TIMEFILE=/tmp/time_file
 NUMFILE=/tmp/num_file
 
 
-
 # Make a 'timefile'
 touch -t ${DATE}0000 ${TIMEFILE}
-
 
 
 # Determines whether to reset the file.
 if [ ! -f ${NUMFILE} ] || [ ${NUMFILE} -ot ${TIMEFILE} ] ; then
     echo '0' >| ${NUMFILE}
 fi
-
 
 
 # Variables 02
@@ -33,10 +29,8 @@ NUM=`printf "%02d " ${NUMNEXT}`
 VALUE=${DATE}${NUM}
 
 
-
 # Run
 echo ${VALUE}
-
 
 
 # Processing at the end.
